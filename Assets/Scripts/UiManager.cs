55c46887db6ba91094b1cfb253a2c415f64b7b30
HukyTax <diego10095200@gmail.com>
2026-04-07 12:23:12 -0700

    Add shop panel open/close UI controls
    Introduce UI controls to toggle a shop panel: add serialized panel reference and cached Button fields (open, close). In Start() the script collects child Buttons and assigns ones named "ToggelOpen" and "ToggelClose". Implement openShop() and closeShop() to move the panel vertically and swap button active states. Also add UnityEngine.UI, TMPro and Unity.Mathematics using directives required for the UI changes.

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,16 +1,48 @@
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
+    private Button close;
+    private Button open;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject panel;
+
     void Start()
     {
-        
+        Button[] buttons = panel.GetComponentsInChildren<Button>();
+        foreach (Button bt in buttons)
+        {
+            if (bt.name == "ToggelOpen")
+            {
+                open = bt;
+            }
+            if(bt.name == "ToggelClose")
+            {
+                close = bt;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void openShop()
+    {
+        panel.transform.position = new Vector3(panel.transform.position.x, panel.transform.position.y + 5);
+        open.gameObject.setActive(false);
+        close.gameObject.setActive(true);
+    }
+    public void closeShop()
+    {
+        panel.transform.position = new Vector3(panel.transform.position.x, panel.transform.position.y - 5);
+        open.gameObject.setActive(true);
+        close.gameObject.setActive(false);
     }
 }
